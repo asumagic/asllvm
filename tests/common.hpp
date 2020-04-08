8590139d389c7adff2a39479cdbb26601c6e32b8
Asu <sdelang@sdelang.fr>
2020-04-08 15:29:15 +0200

    Unify testing logic in one project

---
+++ tests/common.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <angelscript-llvm/detail/assert.hpp>
+#include <angelscript-llvm/jit.hpp>
+#include <angelscript.h>
+#include <sstream>
+#include <string>
+
+#define asllvm_test_check(x)                                                                                           \
+	if (!(x))                                                                                                          \
+	{                                                                                                                  \
+		throw std::runtime_error{"check failed: " #x};                                                                 \
+	}                                                                                                                  \
+	(void)0
+
+extern std::stringstream out;
+
+struct EngineContext
+{
+	EngineContext(asllvm::JitConfig config);
+
+	~EngineContext();
+
+	void register_interface();
+
+	asIScriptModule& build(const char* name, const char* script_path);
+
+	void run(asIScriptModule& module, const char* entry_point);
+
+	asIScriptEngine*     engine;
+	asllvm::JitInterface jit;
+};
+
+std::string run(const char* path, const char* entry = "void main()");
+std::string run_string(const char* str);
---
+++ tests/integermath.cpp
@@ -0,0 +1,18 @@
+#include <catch2/catch.hpp>
+
+#include "common.hpp"
+
+TEST_CASE("Basic integral math", "[intmath]")
+{
+	// Verify signed logic
+	REQUIRE(run_string("print(int8(1) + int8(-2))") == "-1\n");
+	REQUIRE(run_string("print(int16(1) + int16(-2))") == "-1\n");
+	REQUIRE(run_string("print(int32(1) + int32(-2))") == "-1\n");
+	REQUIRE(run_string("print(int64(1) + int64(-2))") == "-1\n");
+
+	// Verify unsigned underflow logic
+	REQUIRE(run_string("print(uint8(1) + uint8(-2))") == "255\n");
+	REQUIRE(run_string("print(uint16(1) + uint16(-2))") == "65535\n");
+	REQUIRE(run_string("print(uint32(1) + uint32(-2))") == "4294967295\n");
+	REQUIRE(run_string("print(uint64(1) + uint64(-2))") == "18446744073709551615\n");
+}
