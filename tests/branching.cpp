0a8b598eb98ce2ea8c4afa775c2b7ca3b4f7fdf1
Asu <sdelang@sdelang.fr>
2020-04-08 16:31:28 +0200

    Added basic flow control tests

---
+++ tests/branching.cpp
@@ -0,0 +1,18 @@
+#include "common.hpp"
+#include <catch2/catch.hpp>
+
+TEST_CASE("basic integral comparisons and 'if'", "[ifcmp]")
+{
+	REQUIRE(run_string("int a = 0; if (a >= 0) { print(1); }") == "1\n");
+	REQUIRE(run_string("int a = 0; if (a < 0) { print(1); }") == "");
+}
+
+TEST_CASE("simple 'for' looping", "[forloop]")
+{
+	REQUIRE(run_string("for (int i = 0; i < 5; ++i) { print(i); }") == "0\n1\n2\n3\n4\n");
+}
+
+TEST_CASE("simple 'while' looping", "[whileloop]")
+{
+	REQUIRE(run_string("int i = 5; while (i != 7) { print(i); ++i; }") == "5\n6\n");
+}
