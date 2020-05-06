51e156d8024564234ca21f048bec0b9246970adf
Asu <sdelang@sdelang.fr>
2020-05-06 17:25:08 +0200

    Added test for shared functions and duplicate names

--- tests/functions.cpp
+++ tests/functions.cpp
@@ -6,3 +6,30 @@ TEST_CASE("references to primitives in parameters", "[refparams]")
 {
 	REQUIRE(run("scripts/refprimitives.as") == "10\n");
 }
+
+TEST_CASE("shared functions", "[shared][sharedfuncs]")
+{
+	EngineContext context(default_jit_config());
+
+	out = {};
+
+	asIScriptModule& module_a = context.build("a", "scripts/sharedfuncs.as");
+	asIScriptModule& module_b = context.build("b", "scripts/sharedfuncs.as");
+
+	context.prepare_execution();
+
+	asIScriptFunction* entry_a = module_a.GetFunctionByDecl("void main()");
+	asIScriptFunction* entry_b = module_b.GetFunctionByDecl("void main()");
+
+	asIScriptContext* script_context = context.engine->CreateContext();
+
+	const auto run = [&](asIScriptFunction* func) {
+		asllvm_test_check(script_context->Prepare(func) >= 0);
+		asllvm_test_check(script_context->Execute() == asEXECUTION_FINISHED);
+	};
+
+	run(entry_a);
+	run(entry_b);
+
+	REQUIRE(out.str() == "10\n10\n");
+}
---
+++ tests/scripts/sharedfuncs.as
@@ -0,0 +1,9 @@
+shared int calc(int a, int b)
+{
+    return a * b;
+}
+
+void main()
+{
+    print(calc(5, 2));
+}
